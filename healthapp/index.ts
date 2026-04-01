90ea8f16a3141fc68a1a4022a332f46b98861c3a
JoeT1327 <thantziaung.joe631@gmail.com>
2026-04-01 14:45:28 +0630

    exercise 8

--- healthapp/index.ts
+++ healthapp/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 
 import { calculateBmi } from "./bmiCalculator.ts";
+import { calculateExercises } from "./exerciseCalculator.ts";
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
@@ -27,7 +30,45 @@ app.get("/bmi", (_req, res) => {
   });
 });
 
-const PORT = 3003;
+app.post("/exercises", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { daily_exercises, target } = req.body;
+
+  if (!daily_exercises || target === undefined) {
+    return res.status(400).json({
+      error: "parameters missing",
+    });
+  }
+
+  if (isNaN(Number(target))) {
+    return res.status(400).json({
+      error: "malformatted parameters",
+    });
+  }
+
+  if (!Array.isArray(daily_exercises)) {
+    return res.status(400).json({
+      error: "malformatted parameters",
+    });
+  }
+
+  if (daily_exercises.some((value: unknown) => isNaN(Number(value)))) {
+    return res.status(400).json({
+      
+      error: "malformatted parameters",
+    });
+    
+  }
+
+  const dailyHours = daily_exercises.map((value: unknown) => Number(value));
+  const targetNumber = Number(target);
+
+  const result = calculateExercises(dailyHours, targetNumber);
+
+  return res.json(result);
+});
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- lecture-exercises/index.ts
+++ lecture-exercises/index.ts
@@ -1,9 +1,23 @@
 import express from "express";
-
+import { calculator } from "./multiplier.ts";
 const app = express();
 
 app.get("/ping", (_req, res) => {
   res.send("pong");
+});
+
+app.post("/calculate", (req, res) => {
+  const { value1, value2, op } = req.body;
+
+  if (isNaN(Number(value1)) || isNaN(Number(value2))) {
+    return res.status(400).json({
+      error: "malformatted parameters",
+    });
+  }
+
+  const result = calculator(Number(value1), Number(value2), op);
+
+  return res.json({ result });
 });
 
 const PORT = 3003;
