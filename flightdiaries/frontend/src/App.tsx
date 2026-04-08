0703517e60eb48b72e6b3d07eed7d94790e9f58a
JoeT1327 <thantziaung.joe631@gmail.com>
2026-04-08 11:02:12 +0630

    exercise 20

--- flightdiaries/frontend/src/App.tsx
+++ flightdiaries/frontend/src/App.tsx
@@ -1,22 +1,74 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import type { DiaryEntry } from "./types";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import type { DiaryEntry, NewDiaryEntry } from './types';
 
 const App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+  const [newDate, setNewDate] = useState('');
+  const [newWeather, setNewWeather] = useState('');
+  const [newVisibility, setNewVisibility] = useState('');
 
   useEffect(() => {
     axios
-      .get<DiaryEntry[]>("http://localhost:3001/api/diaries")
-      .then((response) => {
+      .get<DiaryEntry[]>('http://localhost:3001/api/diaries')
+      .then(response => {
         setDiaries(response.data);
       });
   }, []);
 
+  const addDiary = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    const newEntry: NewDiaryEntry = {
+      date: newDate,
+      weather: newWeather,
+      visibility: newVisibility
+    };
+
+    axios
+      .post<DiaryEntry>('http://localhost:3001/api/diaries', newEntry)
+      .then(response => {
+        setDiaries(diaries.concat(response.data));
+        setNewDate('');
+        setNewWeather('');
+        setNewVisibility('');
+      });
+  };
+
   return (
     <div>
       <h1>Flight Diaries</h1>
-      {diaries.map((diary) => (
+
+      <h2>Add new entry</h2>
+      <form onSubmit={addDiary}>
+        <div>
+          date
+          <input
+            value={newDate}
+            onChange={(e) => setNewDate(e.target.value)}
+          />
+        </div>
+
+        <div>
+          weather
+          <input
+            value={newWeather}
+            onChange={(e) => setNewWeather(e.target.value)}
+          />
+        </div>
+
+        <div>
+          visibility
+          <input
+            value={newVisibility}
+            onChange={(e) => setNewVisibility(e.target.value)}
+          />
+        </div>
+
+        <button type="submit">add</button>
+      </form>
+
+      {diaries.map(diary => (
         <div key={diary.id}>
           <h3>{diary.date}</h3>
           <p>visibility: {diary.visibility}</p>
